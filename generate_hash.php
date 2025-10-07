a04dca234218f92ee02589cecda5428253eb0b68
x414i <m09211127@it.misuratau.edu.ly>
2025-10-07 14:37:17 +0200

    chore(script): simplify password hash generation script

--- generate_hash.php
+++ generate_hash.php
@@ -1,6 +1,3 @@
 <?php
-// ملف توليد كلمة مرور مشفرة لإضافتها في قاعدة البيانات
-$password = '123456789'; // يمكنك تغييرها
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "كلمة المرور المشفرة: <br>" . $hash;
+echo password_hash("password123", PASSWORD_DEFAULT);
 ?>
